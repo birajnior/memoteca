8a851142b96f8940f43dbaf3444d8351d1cfcfec
birajnior <ubiratanbernardo@gmail.com>
2023-08-25 11:28:26 -0300

    utilizando ngIf e ngClass

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -17,6 +17,12 @@ export class ListarPensamentosComponent {
       conteudo: 'Minha propriedade é decorada com @Input()',
       autoria: 'Componente filho',
       modelo: 'modelo1',
-    }
+    },
+    {
+      conteudo:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae deleniti excepturi quas nobis neque, libero quam saepe cupiditate repellendus quasi, laboriosam dolores error nesciunt perspiciatis sunt? Similique corporis debitis beatae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni accusantium repudiandae laudantium, dignissimos excepturi eum ipsa quae assumenda doloribus dolorum distinctio ipsam deleniti nemo minus nihil? Nemo obcaecati dicta earum!',
+      autoria: 'Lorem',
+      modelo: 'modelo2',
+    },
   ];
 }
