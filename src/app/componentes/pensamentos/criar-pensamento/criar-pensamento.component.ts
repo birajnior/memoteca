86d339ead1973bffad72bbf0b378814ae6c85a30
birajnior <ubiratanbernardo@gmail.com>
2023-09-08 22:17:04 -0300

    Finalizando API fake

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Pensamento } from '../pensamentos';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -8,17 +10,22 @@ import { Pensamento } from '../pensamentos';
 })
 export class CriarPensamentoComponent {
   pensamento: Pensamento = {
-    id: 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
+    conteudo: '',
+    autoria: '',
     modelo: 'modelo1',
   };
 
+  constructor(private service: PensamentoService, private router: Router) {}
+
+  ngOnInit(): void {}
+
   criarPensamento() {
-    alert("Novo Pensamento Criado");
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
   }
 
   cancelar() {
-    alert("Cancelou!!!");
+    this.router.navigate(['/listarPensamento']);
   }
 }
