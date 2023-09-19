155a2b56515bf33bf808f6fbe8abb74c01a9c842
birajnior <ubiratanbernardo@gmail.com>
2023-09-18 22:37:49 -0300

    Adicionando as funções dos botões de editar e excluir um pensamento.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "listarPensamento", pathMatch: "full" },
   { path: "criarPensamento", component: CriarPensamentoComponent},
-  { path: "listarPensamento", component: ListarPensamentosComponent}
+  { path: "listarPensamento", component: ListarPensamentosComponent},
+  { path: "pensamentos/excluirPensamento/:id", component: ExcluirPensamentoComponent},
+  { path: "pensamentos/editarPensamento/:id", component: EditarPensamentoComponent}
 ];
 
 @NgModule({
